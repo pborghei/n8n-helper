90d4cbdb591bb27cb1567be64130352c36e3eded
Payam Borghei <payam_borghei@yahoo.com>
2025-03-16 13:56:57 -0400

    Add easy start script on port 8082

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd
+ROOT_PATH=`pwd`
+N8N_ROOT=${ROOT_PATH}/n8n
+DATA_FOLDER=${N8N_ROOT}/sqllite_data
+# using 8082 as external port
+EXTRENAL_PORT=8082
+echo ${ROOT_PATH}
+
+mkdir -p ${N8N_ROOT}
+chmod 777 ${N8N_ROOT}
+mkdir -p ${DATA_FOLDER}
+chmod 777 ${DATA_FOLDER}
+
+docker run -d --rm --name n8n -p ${EXTRENAL_PORT}:5678 -v ${DATA_FOLDER}:/home/node/.n8n docker.n8n.io/n8nio/n8n
+
+echo "n8n accessable on port ${EXTRENAL_PORT}"
